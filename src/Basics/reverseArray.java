66ed94c2d14187b306b108cbadb6ef633c8a734d
RAJENDRA <RAJENDRA@DESKTOP-BJM7M9S>
2023-10-05 00:50:47 +0530

    reverse commit

---
+++ src/Basics/reverseArray.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class reverseArray {
+
+	public static void main(String[] args) 
+	{
+		int[] x= {10,21,32,12,32};
+		
+		for(int i=x.length-1;i>=0;i--)
+		{
+			System.out.println(x[i]);
+		}
+
+	}
+
+}
